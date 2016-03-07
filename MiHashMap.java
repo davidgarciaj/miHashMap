7e9b429d85b9bf4337336259aaaad42e1e9e1589
DavidG <igdrasilenllamas@gmail.com>
2016-03-07 12:49:59 +0100

    Creado metodo size que te indica el tamaño de los arrays

--- MiHashMap.java
+++ MiHashMap.java
@@ -7,27 +7,75 @@
  */
 public class MiHashMap
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    //Contains all the object int
+    private int[] enteros;
+    //Contains all the object String
+    private String[] frases;
 
     /**
-     * Constructor for objects of class MiHashMap
+     * Constructor for objects of class ArrayListInt
      */
     public MiHashMap()
     {
-        // initialise instance variables
-        x = 0;
+        enteros = new int[0];
+        frases = new String[0];
+    }
+    
+    /**
+     * Introduce en el objeto los dos parametros que se relacionan entre si.
+     * @param String clave  añade objeto String a la coleccion
+     * @param int añade objeto int a su coleccion
+     * @return devuelve valor contenido en dicha clave, si no existia devuelve -1
+     */
+    public int put(String clave, int valor){
+        int aux = -1;        
+        boolean exist = false;
+        int cont = 0;
+        while(cont < enteros.length && !exist){
+            if(frases[cont] == clave){exist = true;}
+            cont++;
+        }
+        if(exist){ 
+            aux = enteros[cont-1];
+            enteros[cont-1] = valor;
+        }
+        else{
+            frases[cont] = clave;
+            enteros[cont] = valor;
+        }
+        return aux;
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * Devuelve el elemento con la clave indicada
+     * @param index posición en la que se encuentra el elemento
+     * @returm elemento que se encuentra en dicha posición, de no haberlo devuelve -1
      */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public int get(String clave){
+        int aux = -1;        
+        boolean exist = false;
+        int cont = 0;
+        while(cont < enteros.length && !exist){
+            if(frases[cont] == clave){exist = true;}
+            cont++;
+        }
+        if(exist){ aux = enteros[cont-1];}
+        return aux;
     }
+
+    /**
+     * Indica si existe un elemento en esa posición
+     * @return devuelve true si encuentra elementos en la lista
+     */
+    public boolean isEmpty(){       
+        return (frases.length == 0);
+    }
+
+    /**
+     * Devuelve el tamaño total de los arrays
+     */
+    public int size(){
+        return enteros.length;
+    }
+
 }
